bbac13ef1d610c80d8bb271c2bf0e4b1513e2b03
Yafan <piskbro@163.com>
2022-03-13 09:46:57 +0800

    Currect lab1 file modified

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -1,28 +1,27 @@
-/**
- * Class that prints the Collatz sequence starting from a given number.
- *
- * @author Yafan Shi
+/** Class that prints the Collatz sequence starting from a given number.
+ *  @author YOUR NAME HERE
  */
 public class Collatz {
 
-  /** Buggy implementation of nextNumber! */
-  public static int nextNumber(int n) {
-    if (n == 128) {
-      return 1;
-    } else if (n == 5) {
-      return 3 * n + 1;
-    } else {
-      return n * 2;
+    /** Buggy implementation of nextNumber! */
+    public static int nextNumber(int n) {
+        if (n  == 128) {
+            return 1;
+        } else if (n == 5) {
+            return 3 * n + 1;
+        } else {
+            return n * 2;
+        }
     }
-  }
 
-  public static void main(String[] args) {
-    int n = 5;
-    System.out.print(n + " ");
-    while (n != 1) {
-      n = nextNumber(n);
-      System.out.print(n + " ");
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.print(n + " ");
+        while (n != 1) {
+            n = nextNumber(n);
+            System.out.print(n + " ");
+        }
+        System.out.println();
     }
-    System.out.println();
-  }
 }
+
