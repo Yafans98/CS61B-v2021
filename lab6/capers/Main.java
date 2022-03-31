dc1bd6b32b50dabed9adf33ad3758c4f0e72f1c2
Yafan <piskbro@163.com>
2022-03-31 22:44:12 +0800

    Lab6 finished!

--- lab6/capers/Main.java
+++ lab6/capers/Main.java
@@ -1,7 +1,7 @@
 package capers;
 
 import java.io.File;
-import java.util.Arrays;
+
 
 import static capers.Utils.*;
 
@@ -36,7 +36,6 @@ public class Main {
     if (args.length == 0) {
       Utils.exitWithError("Must have at least one argument");
     }
-    System.out.println("args: " + Arrays.toString(args));
     CapersRepository.setupPersistence();
     String text;
     switch (args[0]) {
